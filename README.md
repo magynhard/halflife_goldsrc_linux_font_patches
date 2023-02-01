706faabc9861b8ba7c463cc0128ba56d8ab404f3
magynhard <github@mail.magynhard.de>
2023-02-01 23:00:05 +0100

    make installer bash only

--- README.md
+++ README.md
@@ -9,45 +9,17 @@ Patch unreadable fonts on Half-Life 1 (GoldSrc) based games on Linux.
 * Day of Defeat 1.2
 
 # Installation
-## Automatic installer (under construction)
-### Prerequisites
-The installer is based on ruby. So ensure ruby is already installed on your system.
-
-To check, if it is available, just run the following command in your terminal:
-```bash
-ruby -v
-```
-> ruby 2.7.2p137 (2020-10-01 revision 5445e04352) [x86_64-linux]
-
-
-If you do not get a version response, usually you can install it by using your package manager.
-
-```bash
-# Ubuntu Linux
-sudo apt install ruby
-# Arch Linux / Manjaro
-sudo pacman -S ruby
-# Fedora Linux / CentOS
-sudo dnf install ruby
-```
+## Automatic installer
 ### Install
 Run
 ```bash
-\curl -sSL https://raw.githubusercontent.com/magynhard/halflife_goldsrc_linux_font_patches/master/setup.sh | ACTION=INSTALL bash
+\curl -sSL https://raw.githubusercontent.com/magynhard/halflife_goldsrc_linux_font_patches/master/setup.sh | ACTION=install bash
 ```
 in your terminal to run the automatic installer script.
-
-It will automatically detect your Linux Distro and install everything necessary.
 
 ### Uninstall
 To uninstall you can run the following command:
 
 ```bash
-\curl -sSL https://raw.githubusercontent.com/magynhard/halflife_goldsrc_linux_font_patches/master/setup.sh | ACTION=UNINSTALL bash
+\curl -sSL https://raw.githubusercontent.com/magynhard/halflife_goldsrc_linux_font_patches/master/setup.sh | ACTION=uninstall bash
 ```
-
-## Manual install
-1. Install the fonts from `fonts` of this repo in your distribution.
-2. Ensure that the font cache is cleared by command.
-3. Copy the file(s) of the mod (e.g. `cstrike`) in the same folder of your installation.
-4. Star your game and have fun again!
--- setup.sh
+++ setup.sh
@@ -1,30 +1,111 @@
 #!/usr/bin/env bash
 
-echo
-echo "#########################################"
-echo "## Half-Life GoldSrc Linux Font Patches"
-echo "#########################################"
-echo
-echo "ACTION: ${ACTION}"
+BLACK="\033[30m"
+RED="\033[31m"
+GREEN="\033[32m"
+YELLOW="\033[33m"
+BLUE="\033[34m"
+PINK="\033[35m"
+CYAN="\033[36m"
+WHITE="\033[37m"
+NORMAL="\033[0;39m"
+
+SYM_SUCCESS=$GREEN✔$NORMAL
+SYM_FAIL=$RED✘$NORMAL
+
+
+echo -e
+echo -e "############################################"
+echo -e "## $YELLOW Half-Life GoldSrc Linux Font Patches $NORMAL"
+echo -e "############################################"
+echo -e "$NORMAL"
+echo -e "➜ Action: $GREEN${ACTION}$NORMAL"
 #
 #if [[ "$EUID" = 0 ]]; then
-#    echo "(1) already root"
+#    echo -e "(1) already root"
 #else
 #    sudo -k # make sure to ask for password on next sudo
 #    if sudo true; then
-#        echo "(2) correct password"
+#        echo -e "(2) correct password"
 #    else
-#        echo "(3) wrong password"
+#        echo -e "(3) wrong password"
 #        exit 1
 #    fi
 #fi
 
+echo -e "➜ Loading files ..."
+
 cd /tmp
-mkdir hglfp
-cd hglfp
-curl -O https://raw.githubusercontent.com/magynhard/halflife_goldsrc_linux_font_patches/master/setup.rb
+rm -rf hl_font_patches
+mkdir hl_font_patches
+cd hl_font_patches
 
-curl https://raw.githubusercontent.com/magynhard/halflife_goldsrc_linux_font_patches/master/cstrike/resource/TrackerScheme.res -o TrackerScheme.res.cstrike
-curl https://raw.githubusercontent.com/magynhard/halflife_goldsrc_linux_font_patches/master/dod/resource/TrackerScheme.res -o TrackerScheme.res.dod
-ruby setup.rb
+curl -sS https://raw.githubusercontent.com/magynhard/halflife_goldsrc_linux_font_patches/master/fonts/Retro%20Gaming.ttf -o RetroGaming.ttf
+curl -sS https://raw.githubusercontent.com/magynhard/halflife_goldsrc_linux_font_patches/master/cstrike/resource/TrackerScheme.res -o TrackerScheme.res.cstrike
+curl -sS https://raw.githubusercontent.com/magynhard/halflife_goldsrc_linux_font_patches/master/dod/resource/TrackerScheme.res -o TrackerScheme.res.dod
+echo
 
+STEAM_PATH=~/.local/share/Steam
+STEAM_PATH_CSTRIKE=${STEAM_PATH}/steamapps/common/Half-Life/cstrike
+STEAM_PATH_DOD=${STEAM_PATH}/steamapps/common/Half-Life/dod
+
+FONT_DESTINATION=/usr/share/fonts/Retro\ Gaming.ttf
+
+CSTRIKE_DESTINATION=$STEAM_PATH_CSTRIKE/TrackerScheme.res
+DOD_DESTINATION=$STEAM_PATH_DOD/TrackerScheme.res
+
+
+echo -e "➜ installing font ..."
+sudo rm -rf "$FONT_DESTINATION"
+sudo cp ./RetroGaming.ttf "$FONT_DESTINATION"
+if [ -f "$FONT_DESTINATION" ]; then
+  sudo fc-cache -f
+  echo -e "  $SYM_SUCCESS installed successfully"
+else
+  echo -e "  $SYM_FAIL error when installing"
+  exit 1
+fi
+echo
+
+echo -e "➜ locating Steam installation ..."
+if [ -d "$STEAM_PATH" ]; then
+    echo -e "  $SYM_SUCCESS Steam found at ${STEAM_PATH}"
+    echo
+    # CSTRIKE
+    echo -e "  ➜ locating Counter-Strike 1.6 installation ..."
+    if [ -d "$STEAM_PATH_CSTRIKE" ]; then
+        echo -e "    $SYM_SUCCESS Counter-Strike 1.6 found at ${STEAM_PATH_CSTRIKE}"
+        echo -e "    ➜ installing scheme ..."
+        rm -rf $CSTRIKE_DESTINATION
+        cp ./TrackerScheme.res.cstrike $CSTRIKE_DESTINATION
+        if [ -f "$CSTRIKE_DESTINATION" ]; then
+          echo -e "      $SYM_SUCCESS installed successfully"
+        else
+          echo -e "      $SYM_FAIL error when installing"
+        fi
+    else
+        echo -e "    $SYM_FAIL Counter-Strike 1.6 not found. Skipping ..."
+    fi
+    echo
+    # DOD
+    echo -e "  ➜ locating Day of Defeat 1.2 installation ..."
+    if [ -d "$STEAM_PATH_DOD" ]; then
+        echo -e "    $SYM_SUCCESS Day of Defeat 1.2 found at ${STEAM_PATH_CSTRIKE}"
+        echo -e "    ➜ installing scheme ..."
+        rm -rf $DOD_DESTINATION
+        cp ./TrackerScheme.res.cstrike $DOD_DESTINATION
+        if [ -f "$DOD_DESTINATION" ]; then
+          echo -e "      $SYM_SUCCESS installed successfully"
+        else
+          echo -e "      $SYM_FAIL error when installing"
+        fi
+    else
+        echo -e "    $SYM_FAIL Day of Defeat 1.2 not found. Skipping ..."
+    fi
+else
+    echo -e "  $SYM_FAIL Steam not found. Aborting ..."
+    exit 1
+fi
+
+echo
+echo -e "$SYM_SUCCESS Installation script finished"
