b47f1f4c1792751a8007449e02986bfa9613b91d
magynhard <github@mail.magynhard.de>
2023-02-05 17:30:52 +0100

    improve fonts on cstrike and dod by backuping and replacing original ClientScheme files

--- setup.sh
+++ setup.sh
@@ -30,8 +30,10 @@ STEAM_PATH_DOD=${STEAM_PATH}/steamapps/common/Half-Life/dod
 
 FONT_DESTINATION=/usr/share/fonts/Retro\ Gaming.ttf
 
-CSTRIKE_DESTINATION=$STEAM_PATH_CSTRIKE/resource/TrackerScheme.res
-DOD_DESTINATION=$STEAM_PATH_DOD/resource/TrackerScheme.res
+CSTRIKE_DESTINATION_1=$STEAM_PATH_CSTRIKE/resource/ClientScheme.res
+CSTRIKE_DESTINATION_2=$STEAM_PATH_CSTRIKE/resource/TrackerScheme.res
+DOD_DESTINATION_1=$STEAM_PATH_DOD/resource/ClientScheme.res
+DOD_DESTINATION_2=$STEAM_PATH_DOD/resource/TrackerScheme.res
 
 
 
@@ -44,8 +46,12 @@ if [ "$ACTION" == "install" ]; then
   cd hl_font_patches
 
   curl -sS https://raw.githubusercontent.com/magynhard/halflife_goldsrc_linux_font_patches/master/fonts/Retro%20Gaming.ttf -o RetroGaming.ttf
+
   curl -sS https://raw.githubusercontent.com/magynhard/halflife_goldsrc_linux_font_patches/master/cstrike/resource/TrackerScheme.res -o TrackerScheme.res.cstrike
+  curl -sS https://raw.githubusercontent.com/magynhard/halflife_goldsrc_linux_font_patches/master/cstrike/resource/ClientScheme.res -o ClientScheme.res.cstrike
+
   curl -sS https://raw.githubusercontent.com/magynhard/halflife_goldsrc_linux_font_patches/master/dod/resource/TrackerScheme.res -o TrackerScheme.res.dod
+  curl -sS https://raw.githubusercontent.com/magynhard/halflife_goldsrc_linux_font_patches/master/dod/resource/ClientScheme.res -o ClientScheme.res.dod
   echo
 
 
@@ -69,13 +75,27 @@ if [ "$ACTION" == "install" ]; then
       echo -e "➜ Locating Counter-Strike 1.6 installation ..."
       if [ -d "$STEAM_PATH_CSTRIKE" ]; then
           echo -e "  $SYM_SUCCESS Counter-Strike 1.6 found at ${STEAM_PATH_CSTRIKE}"
-          echo -e "  ➜ Installing scheme ..."
-          rm -rf $CSTRIKE_DESTINATION
-          cp ./TrackerScheme.res.cstrike $CSTRIKE_DESTINATION
-          if [ -f "$CSTRIKE_DESTINATION" ]; then
-            echo -e "    $SYM_SUCCESS Installed successfully"
+          echo -e "  ➜ Installing ClientScheme ..."
+          if [ -f "${CSTRIKE_DESTINATION_1}.backup" ]; then
+            echo -e "    $SYM_FAIL Backup of ClientScheme exists already"
           else
-            echo -e "    $SYM_FAIL Error when installing"
+            mv $CSTRIKE_DESTINATION_1 ${CSTRIKE_DESTINATION_1}.backup
+            echo -e "    $SYM_SUCCESS Created backup of original ClientScheme"
+          fi
+          rm -rf $CSTRIKE_DESTINATION_1
+          cp ./ClientScheme.res.cstrike $CSTRIKE_DESTINATION_1
+          if [ -f "$CSTRIKE_DESTINATION_1" ]; then
+            echo -e "    $SYM_SUCCESS Installed ClientScheme successfully"
+          else
+            echo -e "    $SYM_FAIL Error when installing ClientScheme"
+          fi
+          echo -e "  ➜ Installing TrackerScheme ..."
+          rm -rf $CSTRIKE_DESTINATION_2
+          cp ./TrackerScheme.res.cstrike $CSTRIKE_DESTINATION_2
+          if [ -f "$CSTRIKE_DESTINATION_2" ]; then
+            echo -e "    $SYM_SUCCESS Installed TrackerScheme successfully"
+          else
+            echo -e "    $SYM_FAIL Error when installing TrackerScheme"
           fi
       else
           echo -e "  $SYM_FAIL Counter-Strike 1.6 not found. Skipping ..."
@@ -84,14 +104,28 @@ if [ "$ACTION" == "install" ]; then
       # DOD
       echo -e "➜ Locating Day of Defeat 1.2 installation ..."
       if [ -d "$STEAM_PATH_DOD" ]; then
-          echo -e "  $SYM_SUCCESS Day of Defeat 1.2 found at ${STEAM_PATH_CSTRIKE}"
-          echo -e "  ➜ Installing scheme ..."
-          rm -rf $DOD_DESTINATION
-          cp ./TrackerScheme.res.cstrike $DOD_DESTINATION
-          if [ -f "$DOD_DESTINATION" ]; then
-            echo -e "    $SYM_SUCCESS Installed successfully"
+          echo -e "  $SYM_SUCCESS Day of Defeat 1.2 found at ${STEAM_PATH_DOD}"
+          echo -e "  ➜ Installing ClientScheme ..."
+          if [ -f "${DOD_DESTINATION_1}.backup" ]; then
+            echo -e "    $SYM_FAIL Backup of ClientScheme exists already"
           else
-            echo -e "    $SYM_FAIL Error when installing"
+            mv $DOD_DESTINATION_1 ${DOD_DESTINATION_1}.backup
+            echo -e "    $SYM_SUCCESS Created backup of original ClientScheme"
+          fi
+          rm -rf $DOD_DESTINATION_1
+          cp ./ClientScheme.res.cstrike $DOD_DESTINATION_1
+          if [ -f "$DOD_DESTINATION_1" ]; then
+            echo -e "    $SYM_SUCCESS Installed ClientScheme successfully"
+          else
+            echo -e "    $SYM_FAIL Error when installing ClientScheme"
+          fi
+          echo -e "  ➜ Installing TrackerScheme ..."
+          rm -rf $DOD_DESTINATION_2
+          cp ./TrackerScheme.res.cstrike $DOD_DESTINATION_2
+          if [ -f "$DOD_DESTINATION_2" ]; then
+            echo -e "    $SYM_SUCCESS Installed TrackerScheme successfully"
+          else
+            echo -e "    $SYM_FAIL Error when installing TrackerScheme"
           fi
       else
           echo -e "  $SYM_FAIL Day of Defeat 1.2 not found. Skipping ..."
@@ -119,35 +153,87 @@ elif [ "$ACTION" == "uninstall" ]; then
     fi
     echo
     # CSTRIKE
-    echo -e "➜ Locating Counter-Strike 1.6 scheme installation ..."
-    if [ -f "$CSTRIKE_DESTINATION" ]; then
-      echo -e "  $SYM_SUCCESS Scheme found at ${CSTRIKE_DESTINATION}"
-      echo -e "  ➜ Uninstalling scheme ..."
-      rm -rf "$CSTRIKE_DESTINATION"
-      if [ -f "$CSTRIKE_DESTINATION" ]; then
-        echo -e "    $SYM_FAIL Error when uninstalling"
-        exit 1
+    echo -e "➜ Locating Counter-Strike 1.6 installation ..."
+    if [ -d "$STEAM_PATH_CSTRIKE" ]; then
+      echo -e "  $SYM_SUCCESS Counter-Strike 1.6 found at ${STEAM_PATH_CSTRIKE}"
+      #   ClientScheme
+      echo -e "  ➜ Uninstalling ClientScheme ..."
+      if [ -f "${CSTRIKE_DESTINATION_1}.backup" ]; then
+        echo -e "    $SYM_SUCCESS ClientScheme backup found at ${CSTRIKE_DESTINATION_1}.backup"
+        rm -rf "$CSTRIKE_DESTINATION_1"
+        if [ -f "$CSTRIKE_DESTINATION_1" ]; then
+          echo -e "    $SYM_FAIL Error when uninstalling ClientScheme (1/2)"
+          exit 1
+        else
+          mv ${CSTRIKE_DESTINATION_1}.backup $CSTRIKE_DESTINATION_1
+          if [ -f "$CSTRIKE_DESTINATION_1" ]; then
+            echo -e "    $SYM_SUCCESS Uninstalled ClientScheme successfully"
+          else
+            echo -e "    $SYM_FAIL Error when uninstalling ClientScheme (2/2)"
+            exit 1
+          fi
+        fi
       else
-        echo -e "    $SYM_SUCCESS Uninstalled successfully"
+        echo -e "    $SYM_FAIL ClientScheme backup not installed. Skipping ..."
+      fi
+      #   TrackerScheme
+      echo -e "  ➜ Uninstalling TrackerScheme ..."
+      if [ -f "$CSTRIKE_DESTINATION_2" ]; then
+        echo -e "    $SYM_SUCCESS TrackerScheme found at ${CSTRIKE_DESTINATION_2}"
+        rm -rf "$CSTRIKE_DESTINATION_2"
+        if [ -f "$CSTRIKE_DESTINATION_2" ]; then
+          echo -e "    $SYM_FAIL Error when uninstalling TrackerScheme"
+          exit 1
+        else
+          echo -e "    $SYM_SUCCESS Uninstalled TrackerScheme successfully"
+        fi
+      else
+        echo -e "    $SYM_FAIL TrackerScheme not installed. Skipping ..."
       fi
     else
-      echo -e "  $SYM_FAIL Scheme not installed. Skipping ..."
+        echo -e "  $SYM_FAIL Counter-Strike 1.6 not found. Skipping ..."
     fi
     echo
     # DOD
-    echo -e "➜ Locating Day of Defeat 1.2 scheme installation ..."
-    if [ -f "$DOD_DESTINATION" ]; then
-      echo -e "  $SYM_SUCCESS Scheme found at ${DOD_DESTINATION}"
-      echo -e "  ➜ Uninstalling scheme ..."
-      rm -rf "$DOD_DESTINATION"
-      if [ -f "$DOD_DESTINATION" ]; then
-        echo -e "    $SYM_FAIL Error when uninstalling"
-        exit 1
+    echo -e "➜ Locating Day of Defeat 1.2 installation ..."
+    if [ -d "$STEAM_PATH_DOD" ]; then
+      echo -e "  $SYM_SUCCESS Counter-Strike 1.6 found at ${STEAM_PATH_DOD}"
+      #   ClientScheme
+      echo -e "  ➜ Uninstalling ClientScheme ..."
+      if [ -f "${DOD_DESTINATION_1}.backup" ]; then
+        echo -e "    $SYM_SUCCESS ClientScheme backup found at ${DOD_DESTINATION_1}.backup"
+        rm -rf "$DOD_DESTINATION_1"
+        if [ -f "$DOD_DESTINATION_1" ]; then
+          echo -e "    $SYM_FAIL Error when uninstalling ClientScheme (1/2)"
+          exit 1
+        else
+          mv ${DOD_DESTINATION_1}.backup $DOD_DESTINATION_1
+          if [ -f "$DOD_DESTINATION_1" ]; then
+            echo -e "    $SYM_SUCCESS Uninstalled ClientScheme successfully"
+          else
+            echo -e "    $SYM_FAIL Error when uninstalling ClientScheme (2/2)"
+            exit 1
+          fi
+        fi
       else
-        echo -e "    $SYM_SUCCESS Uninstalled successfully"
+        echo -e "    $SYM_FAIL ClientScheme backup not installed. Skipping ..."
+      fi
+      #   TrackerScheme
+      echo -e "  ➜ Uninstalling TrackerScheme ..."
+      if [ -f "$DOD_DESTINATION_2" ]; then
+        echo -e "    $SYM_SUCCESS TrackerScheme found at ${DOD_DESTINATION_2}"
+        rm -rf "$DOD_DESTINATION_2"
+        if [ -f "$DOD_DESTINATION_2" ]; then
+          echo -e "    $SYM_FAIL Error when uninstalling TrackerScheme"
+          exit 1
+        else
+          echo -e "    $SYM_SUCCESS Uninstalled TrackerScheme successfully"
+        fi
+      else
+        echo -e "    $SYM_FAIL TrackerScheme not installed. Skipping ..."
       fi
     else
-      echo -e "  $SYM_FAIL Scheme not installed. Skipping ..."
+        echo -e "  $SYM_FAIL Day of Defeat 1.2 not found. Skipping ..."
     fi
 else
   echo "Unknown action '$ACTION'. Aborting ..."
